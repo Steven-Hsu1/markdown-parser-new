5b0796d4ce662a3cff3d8c75e8fe1f95dc990afc
Steven Hsu <103228319+Steven-Hsu1@users.noreply.github.com>
2022-06-05 09:30:45 -0700

    Add files via upload

---
+++ MarkdownParse.java
@@ -0,0 +1,52 @@
+//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class MarkdownParse {
+
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+            int openBracket = markdown.indexOf("[", currentIndex);
+
+            if (openBracket == -1) break;
+
+            while (markdown.charAt(openBracket == 0 ? openBracket : openBracket - 1) == '!' || 
+                markdown.charAt(openBracket == 0 ? openBracket : openBracket - 1) == '\\') {
+                openBracket = markdown.indexOf("[", openBracket + 1);
+
+                if (openBracket == -1) break;
+            }
+
+
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            while (openParen < markdown.length() && markdown.charAt(openParen + 1) == '(') {
+                openParen += 1;
+            }
+            int closeParen = markdown.indexOf(")", openParen);
+
+            if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1) break;
+
+            if (openParen == closeBracket+1) toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
+        }
+
+        return toReturn;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
+        // change
+
+    }
+}
---
+++ MarkdownParseTest.java
@@ -0,0 +1,199 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+    import java.util.Arrays;
+
+    import org.junit.*;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MarkdownParseTest {
+
+    // @Test
+    // public void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+
+    // @Test
+    // public void testFile1() throws IOException {
+
+    //     String content = Files.readString(Path.of("test-file.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>(Arrays.asList("https://something.com", "some-thing.html"));
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+
+
+    // @Test
+    // public void testFile2() throws IOException {
+
+    //     String content = Files.readString(Path.of("test-file2.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>(Arrays.asList("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+
+
+    // @Test
+    // public void testFile3() throws IOException {
+
+    //     String content = Files.readString(Path.of("test-file3.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>(Arrays.asList("https://something.com", "some-thing.html"));
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+
+
+    // @Test
+    // public void testFile4() throws IOException {
+
+    //     String content = Files.readString(Path.of("test-file4.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>(Arrays.asList("actual_link.html"));
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void testFile5() throws IOException {
+
+    //     String content = Files.readString(Path.of("test-file5.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>();
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+
+    // @Test
+    // public void p6TestFile1() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>(Arrays.asList("https://something.com", "some-thing.html"));
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void p6TestFile2() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file2.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>(Arrays.asList("https://something.com", "some-page.html"));
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void p6TestFile3() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file3.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>();
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void p6TestFile4() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file4.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>();
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void p6TestFile5() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file5.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>();
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void p6TestFile6() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file6.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>();
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void p6TestFile7() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file7.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>();
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    // }
+
+    // @Test
+    // public void p6TestFile8() throws IOException {
+
+    //     String content = Files.readString(Path.of("part6/test-file8.md"));
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    //     ArrayList<String> result = new ArrayList<>(Arrays.asList("a link on the first line"));
+
+    //     assertArrayEquals(result.toArray(), links.toArray());
+    //     // change
+    // }
+    @Test
+	public void snippet1() throws IOException {
+		String content = Files.readString(Path.of("lab4-test1.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("`google.com", "google.com", "ucsd.edu"));
+
+        assertArrayEquals(result.toArray(), links.toArray());
+	}
+	@Test
+	public void snippet2() throws IOException {
+		String content = Files.readString(Path.of("lab4-test2.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("a.com", "a.com(())", "example.com"));
+
+        assertArrayEquals(result.toArray(), links.toArray());
+	}
+	@Test
+	public void snippet3() throws IOException {
+		String content = Files.readString(Path.of("lab4-test3.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"));
+
+        assertArrayEquals(result.toArray(), links.toArray());
+	}
+    
+}
